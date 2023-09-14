5ed49c10a6d60e3b06c3f4188cf34d376802b990
Calvin Tandika <calvintandikaa68@gmail.com>
2023-09-14 16:18:51 +0700

    Upgrade version

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Homepage/home'
-import './main.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Homepage/home";
+import "./main.scss";
 
-import { pages } from './data/pages'
+import { pages } from "./data/pages";
 
 const routesList = pages.map((item) => {
-  return (
-  <Route key={item.link} path={item.link} element={item.page} />
-  )
-})
+  return <Route key={item.link} path={item.link} element={item.page} />;
+});
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {routesList}
-      </Routes>
-    </BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {routesList}
+    </Routes>
+  </BrowserRouter>
+);
--- src/pages/PasswordGenerator/pwgen.jsx
+++ src/pages/PasswordGenerator/pwgen.jsx
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Card, Checkbox, Slider, Space, Typography } from 'antd';
-import { HomeOutlined } from '@ant-design/icons';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Button, Card, Checkbox, Slider, Space, Typography } from "antd";
+import { HomeOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
 function PWGEN() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [length, setLength] = useState(12);
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState("");
   const [useUppercase, setUseUppercase] = useState(true);
   const [useLowercase, setUseLowercase] = useState(true);
   const [useNumbers, setUseNumbers] = useState(true);
   const [useSymbols, setUseSymbols] = useState(true);
 
   function generatePassword() {
-    let charset = '';
-    if (useUppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (useLowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
-    if (useNumbers) charset += '0123456789';
-    if (useSymbols) charset += '`~!@#$%^&*()_-+={[}]|\:;"<,>.?/';
+    let charset = "";
+    if (useUppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (useLowercase) charset += "abcdefghijklmnopqrstuvwxyz";
+    if (useNumbers) charset += "0123456789";
+    if (useSymbols) charset += '`~!@#$%^&*()_-+={[}]|:;"<,>.?/';
 
-    let password = '';
+    let password = "";
     for (let i = 0; i < length; i++) {
       password += charset.charAt(Math.floor(Math.random() * charset.length));
     }
@@ -29,49 +29,99 @@ function PWGEN() {
   function copyPassword() {
     if (password) {
       navigator.clipboard.writeText(password);
-      setPassword("Password copied to clipboard")
+      setPassword("Password copied to clipboard");
 
       setTimeout(() => {
-        setPassword(password)
-      },1500)
+        setPassword(password);
+      }, 1500);
     }
   }
 
   useEffect(() => {
-    generatePassword()
-  },[length])
+    generatePassword();
+  }, [length]);
 
   const formatter = (value) => `Password Length : ${value} digit`;
 
   return (
     <>
-    <div className="centerized">
-      <Button className="back" onClick={() => navigate(-1)}><HomeOutlined />Home</Button>
-      <Card title={<><Typography.Title level={4}>Password Generator</Typography.Title><Typography>Generating a secure password</Typography></>} className='card-app'>
-        <Button size='large' id="clipboard" title="copy" onClick={copyPassword} block>
-          <span id="password">{password}</span>
+      <div className="centerized">
+        <Button className="back" onClick={() => navigate(-1)}>
+          <HomeOutlined />
+          Home
         </Button>
+        <Card
+          title={
+            <>
+              <Typography.Title level={4}>Password Generator</Typography.Title>
+              <Typography>Generating a secure password</Typography>
+            </>
+          }
+          className="card-app"
+        >
+          <Button
+            size="large"
+            id="clipboard"
+            title="copy"
+            onClick={copyPassword}
+            block
+          >
+            <span id="password">{password}</span>
+          </Button>
 
-        <Space direction="vertical" style={{margin:'8px 0px 0px 0px', width: '100%'}}>
-          <Slider min={8} max={20} defaultValue={length} onChange={value => setLength(value)} tooltip={{ formatter, placement:'bottom'}} />
-          <Checkbox checked={useUppercase} id="uppercase" onChange={(e) => setUseUppercase(e.target.checked)}>
-            Uppercase (A-Z)
-          </Checkbox>
-          <Checkbox checked={useLowercase} id="lowercase" onChange={(e) => setUseLowercase(e.target.checked)}>
-            Lowercase (a-z)
-          </Checkbox>
-          <Checkbox checked={useNumbers} id="numbers" onChange={(e) => setUseNumbers(e.target.checked)}>
-            Numbers (0-9)
-          </Checkbox>
-          <Checkbox checked={useSymbols} id="symbols" onChange={(e) => setUseSymbols(e.target.checked)}>
-            Symbols (&$%# etc.)
-          </Checkbox>
-        </Space>
-        <Button className='main-action' type='primary' size='large' title="generate" id="generate" onClick={generatePassword} block>
-          Generate Password
-        </Button>
-      </Card>
-    </div>
+          <Space
+            direction="vertical"
+            style={{ margin: "8px 0px 0px 0px", width: "100%" }}
+          >
+            <Slider
+              min={10}
+              max={20}
+              defaultValue={length}
+              onChange={(value) => setLength(value)}
+              tooltip={{ formatter, placement: "bottom" }}
+            />
+            <Checkbox
+              checked={useUppercase}
+              id="uppercase"
+              onChange={(e) => setUseUppercase(e.target.checked)}
+            >
+              Uppercase (A-Z)
+            </Checkbox>
+            <Checkbox
+              checked={useLowercase}
+              id="lowercase"
+              onChange={(e) => setUseLowercase(e.target.checked)}
+            >
+              Lowercase (a-z)
+            </Checkbox>
+            <Checkbox
+              checked={useNumbers}
+              id="numbers"
+              onChange={(e) => setUseNumbers(e.target.checked)}
+            >
+              Numbers (0-9)
+            </Checkbox>
+            <Checkbox
+              checked={useSymbols}
+              id="symbols"
+              onChange={(e) => setUseSymbols(e.target.checked)}
+            >
+              Symbols (&$%# etc.)
+            </Checkbox>
+          </Space>
+          <Button
+            className="main-action"
+            type="primary"
+            size="large"
+            title="generate"
+            id="generate"
+            onClick={generatePassword}
+            block
+          >
+            Generate Password
+          </Button>
+        </Card>
+      </div>
     </>
   );
 }
