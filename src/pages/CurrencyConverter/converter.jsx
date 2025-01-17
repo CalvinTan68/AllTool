a8eea48d7f1942fddffc6f55a14d920aab3dc18b
Calvin Tandika <calvintandikaa68@gmail.com>
2025-01-17 22:49:58 +0700

    Package updates

--- src/pages/CurrencyConverter/converter.jsx
+++ src/pages/CurrencyConverter/converter.jsx
@@ -1,13 +1,13 @@
 import { LoadingOutlined } from "@ant-design/icons";
 import {
-  Button,
-  Divider,
-  InputNumber,
-  Select,
-  Space,
-  Spin,
-  Typography,
-  message,
+    Button,
+    Divider,
+    InputNumber,
+    Select,
+    Space,
+    Spin,
+    Typography,
+    message,
 } from "antd";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -17,137 +17,146 @@ import { VITE_CURRENCY_CONVERTER } from "../../data/constants";
 import currencies from "../../data/currencies.json";
 
 function CurrencyConverter() {
-  const [messageApi, contextHolder] = message.useMessage();
-  const { Option } = Select;
-  const [baseCurrency, setBaseCurrency] = useState("THB");
-  const [targetCurrency, setTargetCurrency] = useState("IDR");
-  const [baseValue, setBaseValue] = useState(1);
-  const [targetValue, setTargetValue] = useState("");
-  const [loading, setLoading] = useState(false);
+    const [messageApi, contextHolder] = message.useMessage();
+    const { Option } = Select;
+    const [baseCurrency, setBaseCurrency] = useState("MYR");
+    const [targetCurrency, setTargetCurrency] = useState("IDR");
+    const [baseValue, setBaseValue] = useState(1);
+    const [targetValue, setTargetValue] = useState("");
+    const [loading, setLoading] = useState(false);
 
-  function convert() {
-    setLoading(true);
-    axios
-      .get(
-        `${VITE_CURRENCY_CONVERTER}/${baseCurrency}/${targetCurrency}/${baseValue}`
-      )
-      .then((res) => {
-        setTargetValue(res.data.conversion_result);
-        setLoading(false);
-        messageApi.success("Value converted");
-      })
-      .catch((error) => {
-        setLoading(false);
-        console.error(error);
-        messageApi.error("API Error / Data not specified");
-      });
-  }
+    function convert() {
+        setLoading(true);
+        axios
+            .get(
+                `${VITE_CURRENCY_CONVERTER}/${baseCurrency}/${targetCurrency}/${baseValue}`
+            )
+            .then((res) => {
+                setTargetValue(res.data.conversion_result);
+                setLoading(false);
+                messageApi.success("Value converted");
+            })
+            .catch((error) => {
+                setLoading(false);
+                console.error(error);
+                messageApi.error("API Error / Data not specified");
+            });
+    }
 
-  useEffect(() => {
-    convert();
-  }, [targetCurrency]);
+    useEffect(() => {
+        convert();
+    }, [targetCurrency]);
 
-  const loadingIcon = (
-    <LoadingOutlined
-      style={{
-        fontSize: 22,
-      }}
-      spin
-    />
-  );
+    const loadingIcon = (
+        <LoadingOutlined
+            style={{
+                fontSize: 22,
+            }}
+            spin
+        />
+    );
 
-  return (
-    <>
-      {contextHolder}
-      <div className="centerized">
-        <HomeButton />
-        <ContentCard
-          title={"Currency Converter"}
-          description={"Realtime currency converter"}
-        >
-          <Space direction="vertical" className="full-width">
-            <Typography.Text>Currency Value</Typography.Text>
-            <InputNumber
-              placeholder="Input base currency"
-              value={baseValue}
-              onChange={(value) => setBaseValue(value)}
-              formatter={(value) =>
-                `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-              }
-              parser={(value) => value.replace(/\$\s?|(,*)/g, "")}
-              addonBefore={baseCurrency}
-              onPressEnter={convert}
-              size="large"
-              className="full-width"
-            />
-            <Typography.Text>Base Currency</Typography.Text>
-            <Select
-              showSearch
-              placeholder="Select base currency"
-              value={baseCurrency}
-              onChange={(value) => setBaseCurrency(value)}
-              size="large"
-              className="full-width"
-              filterOption={(inputValue, option) =>
-                option.children
-                  .join("")
-                  .toLowerCase()
-                  .includes(inputValue.toLowerCase())
-              }
-            >
-              {currencies.map((currency) => (
-                <Option key={currency.number} value={currency.code}>
-                  {currency.code} - {currency.name}
-                </Option>
-              ))}
-            </Select>
-            <Typography.Text>Target Currency</Typography.Text>
-            <Select
-              showSearch
-              placeholder="Select target currency"
-              value={targetCurrency}
-              onChange={(value) => setTargetCurrency(value)}
-              size="large"
-              className="full-width"
-              filterOption={(inputValue, option) =>
-                option.children
-                  .join("")
-                  .toLowerCase()
-                  .includes(inputValue.toLowerCase())
-              }
-            >
-              {currencies
-                .filter((currency) => currency.code !== baseCurrency)
-                .map((currency) => (
-                  <Option key={currency.number} value={currency.code}>
-                    {currency.code} - {currency.name}
-                  </Option>
-                ))}
-            </Select>
-            <Button
-              type="primary"
-              size="large"
-              className="main-action"
-              onClick={convert}
-              block
-            >
-              Convert
-            </Button>
-          </Space>
-          <Divider>Value</Divider>
-          <Typography.Title level={5} className="b">
-            {loading ? (
-              <Spin indicator={loadingIcon} />
-            ) : (
-              <>
-                {targetCurrency} {parseFloat(targetValue).toLocaleString()}
-              </>
-            )}
-          </Typography.Title>
-        </ContentCard>
-      </div>
-    </>
-  );
+    return (
+        <>
+            {contextHolder}
+            <div className="centerized">
+                <HomeButton />
+                <ContentCard
+                    title={"Currency Converter"}
+                    description={"Realtime currency converter"}
+                >
+                    <Space direction="vertical" className="full-width">
+                        <Typography.Text>Currency Value</Typography.Text>
+                        <InputNumber
+                            placeholder="Input base currency"
+                            value={baseValue}
+                            onChange={(value) => setBaseValue(value)}
+                            formatter={(value) =>
+                                `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                            }
+                            parser={(value) => value.replace(/\$\s?|(,*)/g, "")}
+                            addonBefore={baseCurrency}
+                            onPressEnter={convert}
+                            size="large"
+                            className="full-width"
+                        />
+                        <Typography.Text>Base Currency</Typography.Text>
+                        <Select
+                            showSearch
+                            placeholder="Select base currency"
+                            value={baseCurrency}
+                            onChange={(value) => setBaseCurrency(value)}
+                            size="large"
+                            className="full-width"
+                            filterOption={(inputValue, option) =>
+                                option.children
+                                    .join("")
+                                    .toLowerCase()
+                                    .includes(inputValue.toLowerCase())
+                            }
+                        >
+                            {currencies.map((currency) => (
+                                <Option
+                                    key={currency.number}
+                                    value={currency.code}
+                                >
+                                    {currency.code} - {currency.name}
+                                </Option>
+                            ))}
+                        </Select>
+                        <Typography.Text>Target Currency</Typography.Text>
+                        <Select
+                            showSearch
+                            placeholder="Select target currency"
+                            value={targetCurrency}
+                            onChange={(value) => setTargetCurrency(value)}
+                            size="large"
+                            className="full-width"
+                            filterOption={(inputValue, option) =>
+                                option.children
+                                    .join("")
+                                    .toLowerCase()
+                                    .includes(inputValue.toLowerCase())
+                            }
+                        >
+                            {currencies
+                                .filter(
+                                    (currency) => currency.code !== baseCurrency
+                                )
+                                .map((currency) => (
+                                    <Option
+                                        key={currency.number}
+                                        value={currency.code}
+                                    >
+                                        {currency.code} - {currency.name}
+                                    </Option>
+                                ))}
+                        </Select>
+                        <Button
+                            type="primary"
+                            size="large"
+                            className="main-action"
+                            onClick={convert}
+                            block
+                        >
+                            Convert
+                        </Button>
+                    </Space>
+                    <Divider>Value</Divider>
+                    <Typography.Title level={5} className="b">
+                        {loading ? (
+                            <Spin indicator={loadingIcon} />
+                        ) : (
+                            <>
+                                {targetCurrency}{" "}
+                                {parseFloat(targetValue).toLocaleString()}
+                            </>
+                        )}
+                    </Typography.Title>
+                </ContentCard>
+            </div>
+        </>
+    );
 }
 
 export default CurrencyConverter;
--- src/pages/LinkShortener/linkshort.jsx
+++ src/pages/LinkShortener/linkshort.jsx
@@ -5,119 +5,125 @@ import { useState } from "react";
 import ContentCard from "../../components/contentCard";
 import HomeButton from "../../components/homeButton";
 import {
-  VITE_URL_SHORTENER,
-  VITE_URL_SHORTENER_BK,
-  VITE_URL_SHORTENER_TOKEN,
+    VITE_URL_SHORTENER,
+    VITE_URL_SHORTENER_BK,
+    VITE_URL_SHORTENER_TOKEN,
 } from "../../data/constants";
 
 function LinkShort() {
-  const [messageApi, contextHolder] = message.useMessage();
-  const [url, setUrl] = useState("");
-  const [short, setShort] = useState("");
-  const [loading, setLoading] = useState(false);
+    const [messageApi, contextHolder] = message.useMessage();
+    const [url, setUrl] = useState("");
+    const [short, setShort] = useState("");
+    const [loading, setLoading] = useState(false);
 
-  const body = {
-    long_url: url,
-  };
-  const header = {
-    Authorization: "Bearer " + VITE_URL_SHORTENER_TOKEN,
-    "Content-Type": "application/json",
-  };
+    const body = {
+        long_url: url,
+    };
+    const header = {
+        Authorization: "Bearer " + VITE_URL_SHORTENER_TOKEN,
+        "Content-Type": "application/json",
+    };
 
-  function shortenLink() {
-    setLoading(true);
-    setShort("");
-    axios
-      .post(VITE_URL_SHORTENER, body, { headers: header })
-      .then((response) => {
-        setLoading(false);
-        const link = response.data.link;
-        setShort(link);
-        messageApi.success("Short URL generated");
-      })
-      .catch((error) => {
-        console.error(error);
+    function shortenLink() {
+        setLoading(true);
+        setShort("");
         axios
-          .post(VITE_URL_SHORTENER_BK + "?url=" + url)
-          .then((res) => {
-            setLoading(false);
-            const linkbk = res.data.result.full_short_link;
-            setShort(linkbk);
-          })
-          .catch((error) => {
-            console.error(error);
-            setLoading(false);
-            messageApi.error("No URL provided / API Error");
-          });
-      });
-  }
-  function handleInputChange(event) {
-    setUrl(event.target.value);
-  }
-
-  function copyPassword() {
-    if (short) {
-      navigator.clipboard.writeText(short);
-      messageApi.success("Short URL copied");
+            .post(VITE_URL_SHORTENER, body, { headers: header })
+            .then((response) => {
+                setLoading(false);
+                const link = response.data.link;
+                setShort(link);
+                messageApi.success("Short URL generated");
+            })
+            .catch((error) => {
+                console.error(error);
+                axios
+                    .post(VITE_URL_SHORTENER_BK + "?url=" + url)
+                    .then((res) => {
+                        setLoading(false);
+                        const linkbk = res.data.result.full_short_link;
+                        setShort(linkbk);
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                        setLoading(false);
+                        messageApi.error("No URL provided / API Error");
+                    });
+            });
     }
-  }
+    function handleInputChange(event) {
+        setUrl(event.target.value);
+    }
 
-  const loadingIcon = (
-    <LoadingOutlined
-      style={{
-        fontSize: 22,
-      }}
-      spin
-    />
-  );
+    function copyPassword() {
+        if (short) {
+            navigator.clipboard.writeText(short);
+            messageApi.success("Short URL copied");
+        }
+    }
 
-  return (
-    <>
-      {contextHolder}
-      <div className="centerized">
-        <HomeButton />
-        <ContentCard
-          title={"Link Shortener"}
-          description={"Shortening URL for aesthetic look"}
-        >
-          <Space direction="vertical" className="full-width">
-            <Typography.Text>Input your link here</Typography.Text>
-            <Input
-              placeholder="https://google.com/the-long-parameter"
-              onChange={handleInputChange}
-              className="full-width"
-              onPressEnter={shortenLink}
-              size="large"
-            />
+    const loadingIcon = (
+        <LoadingOutlined
+            style={{
+                fontSize: 22,
+            }}
+            spin
+        />
+    );
 
-            <Button
-              className="main-action"
-              type="primary"
-              onClick={shortenLink}
-              block
-              size="large"
-            >
-              Shorten the link
-            </Button>
+    return (
+        <>
+            {contextHolder}
+            <div className="centerized">
+                <HomeButton />
+                <ContentCard
+                    title={"Link Shortener"}
+                    description={"Shortening URL for aesthetic look"}
+                >
+                    <Space direction="vertical" className="full-width">
+                        <Typography.Text>Input your link here</Typography.Text>
+                        <Input
+                            placeholder="https://google.com/the-long-parameter"
+                            onChange={handleInputChange}
+                            className="full-width"
+                            onPressEnter={shortenLink}
+                            size="large"
+                        />
 
-            {loading && (
-              <Typography>
-                <Spin indicator={loadingIcon} />
-              </Typography>
-            )}
-            {short && (
-              <>
-                <Typography.Text>Your short URL is: </Typography.Text>
-                <Button size="large" onClick={copyPassword} block>
-                  {short}
-                </Button>
-              </>
-            )}
-          </Space>
-        </ContentCard>
-      </div>
-    </>
-  );
+                        <Button
+                            className="main-action"
+                            type="primary"
+                            onClick={shortenLink}
+                            block
+                            size="large"
+                        >
+                            Shorten the link
+                        </Button>
+
+                        {loading && (
+                            <Typography>
+                                <Spin indicator={loadingIcon} />
+                            </Typography>
+                        )}
+                        {short && (
+                            <>
+                                <Typography.Text>
+                                    Your short URL is:{" "}
+                                </Typography.Text>
+                                <Button
+                                    size="large"
+                                    onClick={copyPassword}
+                                    block
+                                >
+                                    {short}
+                                </Button>
+                            </>
+                        )}
+                    </Space>
+                </ContentCard>
+            </div>
+        </>
+    );
 }
 
 export default LinkShort;
