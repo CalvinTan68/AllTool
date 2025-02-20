a8e5fdee13d302d408310ec9d8985c184b7a41ee
Calvin Tandika <calvintandikaa68@gmail.com>
2025-02-20 10:38:17 +0700

    Package updates

--- src/main.jsx
+++ src/main.jsx
@@ -8,20 +8,22 @@ import CenterLoading from "./components/centerLoading";
 import { pages } from "./data/pages";
 
 const routesList = pages.map((item) => {
-  return (
-    <Route
-      key={item.link}
-      path={item.link}
-      element={<Suspense fallback={<CenterLoading />}>{item.page}</Suspense>}
-    />
-  );
+    return (
+        <Route
+            key={item.link}
+            path={item.link}
+            element={
+                <Suspense fallback={<CenterLoading />}>{item.page}</Suspense>
+            }
+        />
+    );
 });
 
 ReactDOM.createRoot(document.getElementById("content")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      {routesList}
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            {routesList}
+        </Routes>
+    </BrowserRouter>
 );
